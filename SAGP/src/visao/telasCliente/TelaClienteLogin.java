6aa8098c1d4f12f71c1092648a1d69109867e39b
Vitoria <vitoriabachmann2006@gmail.com>
2024-09-04 11:51:25 -0300

    Criação da tela Cliente Login

--- SAGP/src/visao/telasCliente/TelaClienteLogin.java
+++ SAGP/src/visao/telasCliente/TelaClienteLogin.java
@@ -32,7 +32,7 @@ public class TelaClienteLogin extends JFrame {
 	 */
 	public TelaClienteLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 630, 407);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
