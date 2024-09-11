97abe8b9fb943908c50fd8acc434e0500aabab1c
Jesus <lucas.r2006@aluno.ifsc.edu.br>
2024-09-11 11:49:14 -0300

    Tela Principal

--- SAGP/src/visao/TelaPrincipal.java
+++ SAGP/src/visao/TelaPrincipal.java
@@ -7,11 +7,17 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import net.miginfocom.swing.MigLayout;
+import visao.telasCliente.TelaClienteCadastro;
+import visao.telasFuncionário.TelaFuncionarioCadastro;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Window.Type;
+import java.awt.Cursor;
+import java.awt.Point;
 
 public class TelaPrincipal extends JFrame {
 
@@ -37,9 +43,10 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
+		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 600);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setForeground(new Color(245, 245, 245));
 		contentPane.setBackground(new Color(230, 230, 230));
@@ -55,6 +62,9 @@ public class TelaPrincipal extends JFrame {
 		JButton btnNewButton = new RoundButton("SOU CLIENTE");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				TelaClienteCadastro telaC = new TelaClienteCadastro();
+				dispose();
+				telaC.setVisible(true);
 			}
 		});
 		btnNewButton.setBackground(new Color(224, 83, 76));
@@ -62,6 +72,13 @@ public class TelaPrincipal extends JFrame {
 		contentPane.add(btnNewButton, "cell 2 2 2 1,growx");
 		
 		JButton btnNewButton_1 = new RoundButton("SOU FUNCIONARIO");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaFuncionarioCadastro telaF = new TelaFuncionarioCadastro();
+				dispose();
+				telaF.setVisible(true);
+			}
+		});
 		btnNewButton_1.setForeground(new Color(245, 245, 245));
 		btnNewButton_1.setBackground(new Color(224, 83, 76));
 		contentPane.add(btnNewButton_1, "cell 5 2,growx");
