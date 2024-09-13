7d22906060024aa0bb764b749efb57508227e1d0
Lucas <lucas.r2006@aluno.ifsc.edu.br>
2024-09-13 08:46:06 -0300

    ConexaoBD e jbc

---
+++ SAGP/src/controle/ConexaoBD.java
@@ -0,0 +1,34 @@
+package controle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+	public static Connection getConexaoMySQL() {
+		Connection connection = null;
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("N foi");
+		}
+		
+		String serverName = "localhost:3306/";
+		String mydatabase = "bsagp";
+		String url = "jdbc:mysql://" + serverName + mydatabase;
+		String user = "root";
+		String password = "aluno";
+		
+		try {
+			connection = DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("N foi");
+		}
+		
+		return connection;
+	}
+}
