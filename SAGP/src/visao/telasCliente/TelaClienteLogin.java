5d672f3061276352003bfe0457afca0654790f7f
Vitoria <vitoriabachmann2006@gmail.com>
2024-09-13 09:58:15 -0300

    Tela Cliente Login

--- SAGP/src/visao/telasCliente/TelaClienteLogin.java
+++ SAGP/src/visao/telasCliente/TelaClienteLogin.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import net.miginfocom.swing.MigLayout;
+import visao.RoundButton;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -53,7 +55,7 @@ public class TelaClienteLogin extends JFrame {
 		contentPane.add(lblNewLabel_5, "cell 5 1");
 		
 		JLabel lblNewLabel_4 = new JLabel("ENTRE NA SUA CONTA:");
-		contentPane.add(lblNewLabel_4, "cell 5 3");
+		contentPane.add(lblNewLabel_4, "cell 5 3,alignx center");
 		
 		JLabel lblNewLabel = new JLabel("Usuário: *");
 		contentPane.add(lblNewLabel, "cell 5 4");
@@ -62,7 +64,7 @@ public class TelaClienteLogin extends JFrame {
 		contentPane.add(textField, "cell 5 5,growx");
 		textField.setColumns(10);
 		
-		JButton btnNewButton_1 = new JButton("Limpar");
+		RoundButton btnNewButton_1 = new RoundButton("Limpar");
 		btnNewButton_1.setBackground(new Color(224, 83, 76));
 		contentPane.add(btnNewButton_1, "cell 6 5");
 		
@@ -76,18 +78,18 @@ public class TelaClienteLogin extends JFrame {
 		contentPane.add(textField_1, "cell 5 7,growx");
 		textField_1.setColumns(10);
 		
-		JButton btnNewButton_2 = new JButton("Limpar");
+		RoundButton btnNewButton_2 = new RoundButton("Limpar");
 		btnNewButton_2.setBackground(new Color(224, 83, 76));
 		contentPane.add(btnNewButton_2, "cell 6 7,alignx left");
 		
 		JLabel lblNewLabel_3 = new JLabel("");
 		contentPane.add(lblNewLabel_3, "cell 7 7");
 		
-		JButton btnNewButton = new JButton("Entrar");
+		RoundButton btnNewButton = new RoundButton("Entrar");
 		btnNewButton.setBackground(new Color(224, 83, 76));
-		contentPane.add(btnNewButton, "cell 5 9");
+		contentPane.add(btnNewButton, "cell 5 9,alignx center");
 		
-		JButton btnNewButton_3 = new JButton("Cancelar");
+		RoundButton btnNewButton_3 = new RoundButton("Cancelar");
 		contentPane.add(btnNewButton_3, "cell 6 11");
 	}
 
