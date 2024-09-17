01fa9a78ff6e6019014ce06f3bbe23a22bb2d895
Vitoria <vitoriabachmann2006@gmail.com>
2024-09-17 11:15:06 -0300

    Correção na tela de cadastro funcionario

--- SAGP/src/visao/telasFuncionário/TelaFuncionarioCadastro.java
+++ SAGP/src/visao/telasFuncionário/TelaFuncionarioCadastro.java
@@ -56,13 +56,33 @@ public class TelaFuncionarioCadastro extends JFrame {
 	 */
 	public TelaFuncionarioCadastro() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 609, 488);
+		setBounds(100, 100, 703, 556);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(230, 230, 230));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new MigLayout("", "[grow][][][][][grow][][][][][][][grow]", "[grow][][][grow][][][][][][][][][grow]"));
+		
+		ImageLabel lblNewLabel = new ImageLabel("");
+		lblNewLabel.setIcon(new ImageIcon(TelaFuncionarioCadastro.class.getResource("/Imagem/Logo.png")));
+		contentPane.add(lblNewLabel, "cell 3 2 9 1,alignx center,growy");
+		
+		JLabel lblNewLabel_1 = new JLabel("CRIE UMA CONTA:");
+		contentPane.add(lblNewLabel_1, "cell 3 4 8 1,alignx center");
+		
+		JLabel lblNewLabel_2 = new JLabel("Usuário:");
+		contentPane.add(lblNewLabel_2, "cell 3 5,alignx left");
+		
+		JLabel lblNewLabel_5 = new JLabel("*");
+		lblNewLabel_5.setForeground(new Color(255, 0, 0));
+		contentPane.add(lblNewLabel_5, "cell 4 5,alignx left");
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setForeground(new Color(128, 128, 128));
+		txtUsuario.setText("Coloque o seu usuario para cadastar");
+		contentPane.add(txtUsuario, "cell 3 6 8 1,growx");
+		txtUsuario.setColumns(10);
 		
 		JButton btnNewButton = new RoundButton("LIMPAR");
 		btnNewButton.setBackground(new Color(224, 83, 76));
@@ -74,30 +94,10 @@ public class TelaFuncionarioCadastro extends JFrame {
 				
 			}
 		});
+		contentPane.add(btnNewButton, "cell 11 6");
 		
-		ImageLabel lblNewLabel = new ImageLabel("");
-		lblNewLabel.setIcon(new ImageIcon(TelaFuncionarioCadastro.class.getResource("/Imagem/Logo.png")));
-		contentPane.add(lblNewLabel, "cell 3 2 9 1,alignx center,growy");
-		
-		JLabel lblNewLabel_1 = new JLabel("CRIE UMA CONTA:");
-		contentPane.add(lblNewLabel_1, "cell 3 4 8 1,alignx center");
-		
-		JLabel lblNewLabel_2 = new JLabel("USUARIO:");
-		contentPane.add(lblNewLabel_2, "cell 3 5,alignx center");
-		
-		JLabel lblNewLabel_5 = new JLabel("*");
-		lblNewLabel_5.setForeground(new Color(255, 0, 0));
-		contentPane.add(lblNewLabel_5, "cell 4 5,alignx left");
-		
-		txtUsuario = new JTextField();
-		txtUsuario.setForeground(new Color(128, 128, 128));
-		txtUsuario.setText("Coloque o seu usuario para cadastar");
-		contentPane.add(txtUsuario, "cell 5 5 6 1,growx");
-		txtUsuario.setColumns(10);
-		contentPane.add(btnNewButton, "cell 11 5");
-		
-		JLabel lblNewLabel_4 = new JLabel("NOME:");
-		contentPane.add(lblNewLabel_4, "cell 3 7,alignx center");
+		JLabel lblNewLabel_4 = new JLabel("Nome:");
+		contentPane.add(lblNewLabel_4, "cell 3 7,alignx left");
 		
 		JLabel lblNewLabel_6 = new JLabel("*");
 		lblNewLabel_6.setForeground(new Color(255, 0, 0));
@@ -106,7 +106,7 @@ public class TelaFuncionarioCadastro extends JFrame {
 		txtNome = new JTextField();
 		txtNome.setForeground(new Color(128, 128, 128));
 		txtNome.setText("Entre com seu nome para cadastar");
-		contentPane.add(txtNome, "cell 5 7 6 1,growx");
+		contentPane.add(txtNome, "cell 3 8 8 1,growx");
 		txtNome.setColumns(10);
 		
 		JButton btnNewButton_5 = new RoundButton("New button");
@@ -118,29 +118,13 @@ public class TelaFuncionarioCadastro extends JFrame {
 		btnNewButton_5.setText("LIMPAR");
 		btnNewButton_5.setBackground(new Color(224, 83, 76));
 		btnNewButton_5.setForeground(new Color(245, 245, 245));
-		contentPane.add(btnNewButton_5, "cell 11 7");
+		contentPane.add(btnNewButton_5, "cell 11 8");
 		
-		JLabel lblNewLabel_3 = new JLabel("SENHA:");
-		contentPane.add(lblNewLabel_3, "cell 3 9,alignx center");
+		JLabel lblNewLabel_3 = new JLabel("Senha:");
+		contentPane.add(lblNewLabel_3, "cell 3 9,alignx left");
 		
 		JLabel lblNewLabel_7 = new JLabel("*");
 		contentPane.add(lblNewLabel_7, "cell 4 9,alignx trailing");
-		
-		txtSenha = new JTextField();
-		txtSenha.setForeground(new Color(128, 128, 128));
-		txtSenha.setText("Entre com uma senha");
-		contentPane.add(txtSenha, "cell 5 9 6 1,growx");
-		txtSenha.setColumns(10);
-		
-		JButton btnNewButton_1 = new RoundButton("LIMPAR");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				txtSenha.setText("");
-			}
-		});
-		btnNewButton_1.setBackground(new Color(224, 83, 76));
-		btnNewButton_1.setForeground(new Color(245, 245, 245));
-		contentPane.add(btnNewButton_1, "cell 11 9");
 		
 		JButton btnNewButton_2 = new RoundButton("CADASTRAR");
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -194,9 +178,25 @@ public class TelaFuncionarioCadastro extends JFrame {
 			
 			}
 		});
+		
+		txtSenha = new JTextField();
+		txtSenha.setForeground(new Color(128, 128, 128));
+		txtSenha.setText("Entre com uma senha");
+		contentPane.add(txtSenha, "cell 3 10 8 1,growx");
+		txtSenha.setColumns(10);
+		
+		JButton btnNewButton_1 = new RoundButton("LIMPAR");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtSenha.setText("");
+			}
+		});
+		btnNewButton_1.setBackground(new Color(224, 83, 76));
+		btnNewButton_1.setForeground(new Color(245, 245, 245));
+		contentPane.add(btnNewButton_1, "cell 11 10");
 		btnNewButton_2.setBackground(new Color(224, 83, 76));
 		btnNewButton_2.setForeground(new Color(245, 245, 245));
-		contentPane.add(btnNewButton_2, "cell 7 10");
+		contentPane.add(btnNewButton_2, "cell 7 11");
 		
 		JButton btnNewButton_4 = new RoundButton("CANCELAR");
 		btnNewButton_4.addActionListener(new ActionListener() {
@@ -210,12 +210,12 @@ public class TelaFuncionarioCadastro extends JFrame {
 		});
 		btnNewButton_4.setBackground(new Color(0, 0, 0));
 		btnNewButton_4.setForeground(new Color(245, 245, 245));
-		contentPane.add(btnNewButton_4, "cell 10 11");
+		contentPane.add(btnNewButton_4, "cell 10 12");
 		
 		JButton btnNewButton_3 = new RoundButton("ENTRAR");
 		btnNewButton_3.setBackground(new Color(224, 83, 76));
 		btnNewButton_3.setForeground(new Color(245, 245, 245));
-		contentPane.add(btnNewButton_3, "cell 11 11");
+		contentPane.add(btnNewButton_3, "cell 11 12");
 	}
 
 }
