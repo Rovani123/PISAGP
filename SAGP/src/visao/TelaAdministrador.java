679896c1f86562f709891873818a0b974e82e866
danielar03 <daniela.r03@aluno.ifsc.edu.br>
2024-09-18 11:25:52 -0300

    Tela Administrador

---
+++ SAGP/src/visao/TelaAdministrador.java
@@ -0,0 +1,58 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class TelaAdministrador extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaAdministrador frame = new TelaAdministrador();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaAdministrador() {
+		setBackground(new Color(230, 230, 230));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 733, 568);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[grow][][][][][grow]", "[grow][][][][grow]"));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(TelaAdministrador.class.getResource("/Imagem/Logo.png")));
+		contentPane.add(lblNewLabel, "cell 2 1 2 1,alignx center,aligny center");
+		
+		JButton btnNewButton = new RoundButton("New button");
+		btnNewButton.setText("ALTERAR A TELA DE FUNCIONARIO");
+		btnNewButton.setForeground(new Color(245, 245, 245));
+		btnNewButton.setBackground(new Color(224, 83, 76));
+		contentPane.add(btnNewButton, "cell 2 3 2 1,growx,aligny center");
+	}
+
+}
