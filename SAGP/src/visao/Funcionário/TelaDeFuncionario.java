51bfc2a0e85d573aeb198ac5bae0b944aeeb323e
danielar03 <daniela.r03@aluno.ifsc.edu.br>
2024-10-01 10:09:52 -0300

    Daniela -Tela de funcionário :)

--- SAGP/src/visao/Funcionário/TelaDeFuncionario.java
+++ SAGP/src/visao/Funcionário/TelaDeFuncionario.java
@@ -66,60 +66,70 @@ public class TelaDeFuncionario extends JFrame {
 		contentPane.setLayout(new MigLayout("", "[grow][][][][][][][][][][grow]", "[][][grow]"));
 		
 		panel = new JPanel();
+		panel.setBackground(new Color(230, 230, 230));
 		contentPane.add(panel, "cell 0 0 1 3,alignx left,growy");
-		panel.setLayout(new MigLayout("", "[][]", "[][][][][][][][][][][][][][][]"));
+		panel.setLayout(new MigLayout("", "[][][][]", "[][][][grow][][grow][][][][][][grow][][][grow]"));
 		
 		lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/LogoPequena.png")));
-		panel.add(lblNewLabel_1, "cell 0 1 2 1,alignx center,aligny center");
+		panel.add(lblNewLabel_1, "cell 0 1 4 1,alignx center,aligny center");
 		
 		lblNewLabel_2 = new JLabel("TABELA DE FUNCIONARIO");
-		panel.add(lblNewLabel_2, "cell 1 4,alignx center,aligny center");
-		
-		lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/troca.png")));
-		panel.add(lblNewLabel_5, "cell 0 6,alignx right");
+		panel.add(lblNewLabel_2, "cell 0 4 4 1,alignx center,aligny center");
 		
 		btnNewButton_2 = new RoundButton("ALTERAR");
+		btnNewButton_2.setForeground(new Color(245, 245, 245));
+		btnNewButton_2.setBackground(new Color(224, 83, 76));
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		panel.add(btnNewButton_2, "cell 1 6");
+		
+		lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/troca.png")));
+		panel.add(lblNewLabel_5, "cell 1 6,alignx right");
+		panel.add(btnNewButton_2, "cell 2 6,alignx left,aligny center");
 		
 		lblNewLabel_4 = new JLabel("");
 		lblNewLabel_4.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/lixeira.png")));
-		panel.add(lblNewLabel_4, "cell 0 7,alignx right");
+		panel.add(lblNewLabel_4, "cell 1 7,alignx right");
 		
 		btnNewButton_3 = new RoundButton("DELETAR");
-		panel.add(btnNewButton_3, "cell 1 7");
+		btnNewButton_3.setForeground(new Color(245, 245, 245));
+		btnNewButton_3.setBackground(new Color(224, 83, 76));
+		panel.add(btnNewButton_3, "cell 2 7,alignx left,aligny center");
 		
 		lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/lupa.png")));
-		panel.add(lblNewLabel_6, "cell 0 8,alignx right");
+		panel.add(lblNewLabel_6, "cell 1 8,alignx right");
 		
 		btnNewButton_4 = new RoundButton("ADICIONAR");
+		btnNewButton_4.setForeground(new Color(245, 245, 245));
+		btnNewButton_4.setBackground(new Color(224, 83, 76));
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		panel.add(btnNewButton_4, "cell 1 8");
+		panel.add(btnNewButton_4, "cell 2 8,alignx left,aligny center");
 		
 		lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/volte.png")));
-		panel.add(lblNewLabel_3, "cell 0 14");
+		panel.add(lblNewLabel_3, "cell 1 13");
 		
 		btnNewButton = new RoundButton("VOLTAR");
-		panel.add(btnNewButton, "cell 1 14");
+		btnNewButton.setForeground(new Color(245, 245, 245));
+		btnNewButton.setBackground(new Color(224, 83, 76));
+		panel.add(btnNewButton, "cell 2 13,alignx left,aligny center");
 		
 		JLabel lblNewLabel = new JLabel("FUNCIONARIO");
 		contentPane.add(lblNewLabel, "cell 1 1,alignx center,aligny center");
 		
 		textField = new JTextField();
-		contentPane.add(textField, "cell 2 1 7 1,growx");
+		contentPane.add(textField, "cell 2 1 7 1,growx,aligny center");
 		textField.setColumns(10);
 		
 		btnNewButton_1 = new RoundButton("");
+		btnNewButton_1.setBackground(new Color(230, 230, 230));
 		btnNewButton_1.setIcon(new ImageIcon(TelaDeFuncionario.class.getResource("/Imagem/lupa.png")));
 		contentPane.add(btnNewButton_1, "cell 9 1,alignx center");
 		
