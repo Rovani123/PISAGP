5f75df97ca3a0cfedb35271e27459c0f2ffde835
LucasiValle <lucas.irvalle@gmail.com>
2024-10-09 12:09:48 -0300

    CadastroF

--- SAGP/src/modelo/dao/FuncionarioDAO.java
+++ SAGP/src/modelo/dao/FuncionarioDAO.java
@@ -1,52 +1,23 @@
 package modelo.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import dal.ConexaoBD;
-import modelo.classes.Funcionario;
 
-public class FuncionarioDAO {
+public class FuncionarioDAO extends ModeloDAO{
 
-	public void CadastrarFuncionario(String nome,String usuario, String senha) {
+	public void CadastrarFuncionario(String nome,String usuario, String senha) throws SQLException {
 
-		Funcionario novoFuncionario = new Funcionario();
-		
-		novoFuncionario.setUsuarioFuncionario(usuario);
-		novoFuncionario.setNome(nome);
-		novoFuncionario.setSenha(senha);
-		
-		String insert = "insert into Funcionarios (nomeFuncionario,usuarioFuncionario,senha,administrador) VALUES (?,?,?,?)";
-		
-		Statement stml = null;
-		Connection conn = ConexaoBD.getConexaoMySQL();
-		
-		try {
-			//stml = (Statement) conn.prepareStatement(insert);
-			PreparedStatement preparedStatement = conn.prepareStatement(insert);
-			//ResultSet resl = null;
-			
-			preparedStatement.setString(1, usuario);
-			preparedStatement.setString(2, nome);
-			preparedStatement.setString(3, senha);
-			preparedStatement.setInt(4,0);
-			
-			
-			int row = preparedStatement.executeUpdate();
-			
-			
-			preparedStatement.close();
-			//resl.close();
-			//stml.close();
-			conn.close();
-			
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			
-			
-		}
+		super.save("insert into Funcionarios (nomeFuncionario,usuarioFuncionario,senha,administrador) VALUES (?,?,?,?)",nome,usuario,senha,0);
 		
 	}
+	private void AlterarFuncionarios(Funcionario f) {
+		super.update("UPDATE funcionarios SET nomeFuncionario = ?, usuarioFuncionario = ?, senha = ?, administrador = ? WHERE idFuncionario = ?",
+				p.getidFuncionario(), p.getnomeFuncionario(),p.getusuarioFuncionario(),p.getsenha(),p.getadministrador().toString());
+	}
 	
+	private void DeletarFuncionario() {
+		
+
+	}
+
 }
+
