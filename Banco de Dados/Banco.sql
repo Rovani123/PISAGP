3dd415e4a32bd09a7614cbee3905351a903a9bb7
Jesus <lucas.r2006@aluno.ifsc.edu.br>
2024-11-05 11:44:48 -0300

    atualizar banco

--- Banco de Dados/Banco.sql
+++ Banco de Dados/Banco.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bsagp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `bsagp`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bsagp
@@ -218,4 +220,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-05 11:29:44
+-- Dump completed on 2024-11-05 11:39:59
